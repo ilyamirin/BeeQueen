909627ff034f334cbb6ecec7d595c3cf401937a4
Ilya G. Mirin <ilya.mirin@gmail.com>
2013-03-29 17:23:25 +1100

    accomplish impressionprocessing

--- lib/BeeQueen.pm
+++ lib/BeeQueen.pm
@@ -3,6 +3,7 @@ use Dancer ':syntax';
 use MongoDB;
 use DateTime;
 use String::Random;
+use Data::Dumper;
 
 our $VERSION = '0.1';
 
@@ -10,28 +11,38 @@ our $random = String::Random->new;
 
 our $client = MongoDB::MongoClient->new( host => 'localhost', port => 27017 );
 our $database = $client->get_database( 'beequeen' );
+
 our $impressions = $database->get_collection( 'impressions' );
 our $clicks = $database->get_collection( 'clicks' );
 our $events = $database->get_collection( 'events' );
 
+our $targets = $database->get_collection( 'targets' );
+our $banners = $database->get_collection( 'banners' );
+
 sub determine_banners_for_impression {
-    my ( $impression ) = @_;
-    my @banners = ( 
-        { 
-            bannerid => $random->randregex('[0-9a-z]{36}'), 
-            banner_picture => 'http://www.google.com/images/srpr/logo4w.png'
-        }
-    );
+    my $impression = shift;
+
+    my $query = { _id => MongoDB::OID->new(value => $impression->{ targetid }) };
+    my $target = $targets->find( $query )->next;
+   
+    #TODO:: show one user no more times than?
+    #TODO:: roulette them?
+
+    $query = { _id => { '$in' => $target->{ banners } } };
+    my @banners = $banners->find( $query )->all;
+    info length( @banners ) . ' banners found for impression';
     \@banners;
 };
 
-sub get_url_for_banner {
-    my $bannerid = shift;
-    return 'http://www.google.com/';
+sub get_url_for_banner_with_id {
+    my $banner = $banners->findOne({ 
+            _id => MongoDB::OID->new(value => shift) 
+    });
+    return $banner->{ url };
 };
 
 get '/' => sub {
-    { name => 'Cloud Advertisement Processor', now => DateTime->now };
+    { name => 'Cloud Advertisement Processor', now => DateTime->now->datetime() };
 };
 
 any [ 'get', 'post' ] => '/impression' => sub {
@@ -40,14 +51,15 @@ any [ 'get', 'post' ] => '/impression' => sub {
     $impr->{ recieved_at } = DateTime->now; 
     $impr->{ banners } = determine_banners_for_impression( $impr );
     $impressions->insert( $impr );
-    return $impr->{ banners };
+    #let client see the banners URLs list
+    return [ map { $_->{ url } } @{ $impr->{ banners } } ];
 };
 
 any [ 'get', 'post' ] => '/click' => sub {
     my $click = {};
     $click->{ $_ } = params->{ $_ } for qw{ userid targetid bannerid };
     $click->{ recieved_at } = DateTime->now; 
-    $click->{ redirected_to } = get_url_for_banner( $click->{ bannerid } ); 
+    $click->{ redirected_to } = get_url_for_banner_with_id( $click->{ bannerid } ); 
     $clicks->insert( $click );
     redirect $click->{ redirected_to };
 };
