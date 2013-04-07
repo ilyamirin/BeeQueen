5083a2984127aa212247dd07706822ca59608eb7
Pavel Pozdniak <pozdpl@gmail.com>
2013-04-07 12:46:30 +1100

    Issue-3 Introduce package TestUtilsMongo, to provide ease creation operations on tests behalf.

---
+++ lib/TestUtils/TestUtilsMongo.pm
@@ -0,0 +1,33 @@
+package TestUtils::TestUtilsMongo;
+
+use Moo;
+
+use MongoDB;
+use MongoDB::MongoClient;
+use MongoDB::OID;
+
+
+ #database handler
+ has 'database' => (is => 'ro',);
+ 
+ sub create_target(){
+ 	my ($self, $target_id, $target_name) = @_;
+ 	my $targets_collection = $self->database->get_collection('targets');
+ 	my $target_oid = $targets_collection
+ 						->insert({ 'target_id' => $target_id, 'target_name' => $target_name });
+ 	return $target_oid;
+ }
+
+sub create_banner(){
+	my ($self, $target_id, $bnanner_url, $banner_prob) = @_;
+ 	my $banners_collection = $self->database->get_collection('banners');
+ 	my $banner_oid = $banners_collection
+ 					->insert({ 'url' => $banner_url, 'prob' => $banner_prob });
+ 	
+ 	my $targets_collection = $self->database->get_collection('targets');
+ 	$targets_collection->update({'tartget_id' => $target_id}, 
+ 								{'$push' => {'banners' => $banner_oid}});
+}
+
+1;
+
