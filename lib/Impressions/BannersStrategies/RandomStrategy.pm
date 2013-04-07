22c99ea4752a052738bdea9e0db8226877b28a67
Pavel Pozdniak <pozdpl@gmail.com>
2013-04-07 21:04:09 +1100

    Issue-3 Fix role inception

--- lib/Impressions/BannersStrategies/RandomStrategy.pm
+++ lib/Impressions/BannersStrategies/RandomStrategy.pm
@@ -4,13 +4,12 @@ use strict;
 use warnings;
 
 use Moo;
-use Moo::Role;
 
 with('Impressions::BannersStrategies::StrategyRole');
 
 sub pick_banner(){
 	my ($self, $banners_list) = @_;
-	$picked_banner = 0;
+	my $picked_banner = 0;
 	if(defined $banners_list){
 		my $list_last_index = $#{banners_list};
 		my $picked_index = int(rand($list_last_index));
