a8ef348ef12e322ed52437a9ddb9d6c83e035394
Pavel Pozdniak <pozdpl@gmail.com>
2013-04-07 21:06:45 +1100

    Issue-3 Variable name/ holly molly

--- lib/Impressions/BannersStrategies/RandomStrategy.pm
+++ lib/Impressions/BannersStrategies/RandomStrategy.pm
@@ -11,10 +11,10 @@ sub pick_banner(){
 	my ($self, $banners_list) = @_;
 	my $picked_banner = 0;
 	if(defined $banners_list){
-		my $list_last_index = $#{banners_list};
+		my $list_last_index = $#{$banners_list};
 		my $picked_index = int(rand($list_last_index));
 		$picked_banner = ${$banners_list}[$picked_index];
 	}
 	
-	return $picket_banner;
+	return $picked_banner;
 }
