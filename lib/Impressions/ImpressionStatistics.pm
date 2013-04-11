6428611fd64dcf3ea4d8e1b1652b335bb86febbe
Pavel Pozdniak <pozdpl@gmail.com>
2013-04-12 07:42:10 +1100

    Issue-3 little class to save impression statistics

--- lib/Impressions/ImpressionStatistics.pm
+++ lib/Impressions/ImpressionStatistics.pm
@@ -21,19 +21,39 @@ has 'database' => (is => 'ro');
 # See Also   : n/a
 sub update_impression_stat(){
 	my ($self, $target_id, $banner_id) = @_;
+	my $hour_status = $self->__update_hour_stat($target_id, $banner_id);
+	my $hour_status = $self->__update_day_stat($target_id, $banner_id);
+	my $hour_status = $self->__update_month_stat($target_id, $banner_id);
 }
 
 sub __update_month_stat(){
 	my ($self, $target_id, $banner_id) = @_;
-    my $month_collection = $self->database->get_collection($BANNERS_COLLECTION_NAME);   
+    my $month_collection = $self->database->get_collection(MONTH_STAT_COLLECTION_NAME);
+    my $status = $month_collection->update({'target_id' => $target_id, 'banner_id' => $banner_id},
+                                {'$inc' => {'count' => 1}},
+                                {'upsert' => 1}
+                            );
+    return $status;   
 }
 
 sub __update_day_stat(){
 	my ($self, $target_id, $banner_id) = @_;
+	my $day_collection = $self->database->get_collection(DAY_STAT_COLLECTION_NAME);
+    my $status = $day_collection->update({'target_id' => $target_id, 'banner_id' => $banner_id},
+                                {'$inc' => {'count' => 1}},
+                                {'upsert' => 1}
+                            );
+    return $status;
 }
 
 sub __update_hour_stat(){
 	my ($self, $target_id, $banner_id) = @_;
+	my $hour_collection = $self->database->get_collection(MONTH_STAT_COLLECTION_NAME);
+    my $status = $hour_collection->update({'target_id' => $target_id, 'banner_id' => $banner_id},
+                                {'$inc' => {'count' => 1}},
+                                {'upsert' => 1}
+                            );
+    return $status;
 }
 
 
