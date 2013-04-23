31d6da8ac0e6db4c20eebf0cbb100b7c68da4e77
Pavel Pozdniak <pozdpl@gmail.com>
2013-04-24 08:10:05 +1100

    Issue-6 Function to obtain all stored events for test purpose.

--- lib/Events/EventsService.pm
+++ lib/Events/EventsService.pm
@@ -16,7 +16,7 @@ use constant EVENTS_COLLECTION_NAME => 'events';
 use constant EVENTS_DESCRIPTION_COLLECTION_NAME => 'events_desciption'; 
 
 #########################################################################
-# Usage      : $status = register_event($event_id, $user_id);
+# Usage      : $status = register_event($event_id,$banner_id, $user_id);
 # Purpose    : register event in database
 # Returns    : status of operation 1 if success and 0 otherwise 
 # Parameters : event_id - id of a target
@@ -30,10 +30,31 @@ sub register_event(){
     my $events_collection = $self->database->get_collection(EVENTS_COLLECTION_NAME);
     my $event_oid = $events_collection->insert({
         'event_id' => MongoDB::OID->new('value' => $event_id), 
+        'banner_id' => MongoDB::OID->new('value' => $banner_id),
         'user_id' => $user_id,
         'time' => time()
     });
     return 1;      
+}
+
+#########################################################################
+# Usage      : @cliks = $events_service->find_by_event($event_id);
+# Purpose    : get all events records from collection by event id
+#              initally this method is targeted to be used only in tests 
+# Returns    : array of hashes ({event_id => '', banner_id => 'sdf', user_id => , time}, ...) 
+# Parameters : event_id - id of an event 
+# Throws     : no exceptions
+# Comments   : ???
+# See Also   : n/a
+sub find_by_event(){
+    my ($self, $event_id) = @_;
+    
+    my $events_collection = $self->database->get_collection(EVENTS_COLLECTION_NAME);
+    my @clicks = $events_collection->find({
+        'event_id' => MongoDB::OID->new('value' => $event_id),
+    })->all();
+    
+    return @clicks;
 }
 
 
