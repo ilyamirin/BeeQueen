e6dcf3fd52dc905f97806883ca79c66f8aea524b
Pavel Pozdniak <pozdpl@gmail.com>
2013-05-06 08:19:38 +1100

    Issue-9 Target banners OIDs array setter

--- lib/Impressions/BannersQueryBuilder.pm
+++ lib/Impressions/BannersQueryBuilder.pm
@@ -4,23 +4,40 @@ package Impressions::BannersQueryBuilder;
 =head1 Impressions::BannersQueryBuilder
 This module provided ability to build different kind of querys for banners 
 extraction, depending on input parameters.
+=head2 Example
+ $query_builder = Impressions::BannersQueryBuilder->new;
+ $query = $query_builder->clear_builder
+                ->set_target_banners($target->{banners})
+                ->build();
 =cut
 
 use warnings;
 use strict;
 use Moo;
 
+#oid of given target banners
+has 'target_banners_oids' => (is => 'rwp');
+
+
+
 ############################################
 # Usage      : $query = $query_builder->build()
 # Purpose    : get resulted query from builder parameters
-# Returns    : hash that can be used as query for mongo db
+# Returns    : hash reference that can be used as query for mongo db
 #               {'target_id' => '1231231kjlkjl1323', 'geo' => 'GB'}
 # Parameters : none
 # Throws     : no exceptions
 # Comments   : ???
 # See Also   : n/a
 sub build(){
+	my ($self) = shift;
 	
+	my %query = ();
+	if(defined $self->target_banners_oids){
+		$query{'_id'} = { '$in' => $self->target_banners_oids};
+	}
+	
+	return \%query;
 }
 
 
@@ -33,7 +50,27 @@ sub build(){
 # Comments   : ???
 # See Also   : n/a
 sub clear_builder(){
+    my ($self) = shift;
+    
+    $self->target_banners_oids = undef;
+    
+    return $self;	
+}
+
+############################################
+# Usage      : $query = $query_builder->set_target_banners($target->{'banners'})
+# Purpose    : set target banners
+# Returns    : $self - this object.
+# Parameters : target banners oid array
+# Throws     : no exceptions
+# Comments   : ???
+# See Also   : n/a
+sub set_target_banners(){
+	my ($self, $target_banners) = @_;
 	
+	$self->set_target_banners_oids($target_banners);
+	
+	return $self;
 }
 
 1;
