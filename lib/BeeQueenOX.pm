af3b9cb201f687a9cffea7b44f9b5d601340b969
Pavel Pozdniak <pozdpl@gmail.com>
2013-09-04 08:16:35 +1100

    Issue-10 wrote dependencies for controller

--- lib/BeeQueenOX.pm
+++ lib/BeeQueenOX.pm
@@ -3,6 +3,14 @@ package BeeQueenOX;
 use OX;
 with('DependencyInjection::DependencyInjectionRole');
 
-
+has controller => (
+    is => 'ro',
+    isa => 'Controllers::AdController',
+    dependencies => {
+    	impression_service => 'impression_service',
+    	clicks_service => 'clicks_service',
+    	events_service => 'events_service',
+    }
+);
 
 1;
